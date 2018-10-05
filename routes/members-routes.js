f0182b29a187e83a06b421daa3952ef2be57f19c
B1605369 <toanb1605369@student.ctu.edu.vn>
2018-10-05 22:12:31 +0700

    Complete the basic validate (required)

--- routes/members-routes.js
+++ routes/members-routes.js
@@ -7,11 +7,12 @@ router.use(bodyParser.urlencoded({extended: true}));
 
 //require controller
 const controller = require('../controllers/members-controller');
+const validator = require('../validations/members/members-validate-input');
 
 
 router.get('/', controller.getMembers);
 router.get('/members-create', controller.getCreateMembers);
 router.get('/members-search', controller.getSearchMembers);
-router.post('/members-create', controller.postCreateMembers);
+router.post('/members-create', validator.inputValidate,controller.postCreateMembers);
 
 module.exports = router;
