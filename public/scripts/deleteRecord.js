d11489a7adadfc5c258ca8c458b37c49cc072e3f
B1605369 <toanb1605369@student.ctu.edu.vn>
2018-10-09 23:06:40 +0700

    Complete deleting a collection

---
+++ public/scripts/deleteRecord.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    
+    //get the id off all delete button
+    let delBtnIds = document.querySelectorAll(".del-button");
+
+    // console.log(delBtnIds);
+    
+    
+    
+    
+    
+    
+    console.log("Loaded!");
+});
